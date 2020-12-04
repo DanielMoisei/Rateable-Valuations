e408ee00532fe7c275a8955dd04b3dfb28c2d01e
DanielMoisei <54266203+DanielMoisei@users.noreply.github.com>
2020-12-04 10:55:46 +0200

    Current version/4.12

--- openLogin.js
+++ openLogin.js
@@ -1,7 +1,37 @@
-function toggleLogin(state) {
-    document.getElementById('overlay').style.display = state;
+function toggleModal(state, id, parentModal) {
+
+  // console.log(parentModal)
+
+  document.getElementById(id).style.display = state;
+
+  if (parentModal == 'undefined' || parentModal == null) {
+    // Daca parentModal nu exista
+    // Facem ca 'close button' sa inchida termsModal
+    document.getElementById('close-button').onclick = document.getElementById(id).style.display = 'none'
+  } else {
+    // Daca parentModal exista
+    // Il inchide pe parent modal intai
+    document.getElementById(parentModal).style.display = 'none';
+    // Si butonul de close va redeschide parintele
+    document.getElementById('close-button').onclick = document.getElementById(parentModal).style.display = 'flex'; document.getElementById(id).style.display = 'none';
+  }
+
+
+
+
+
+
+  if (state == 'flex') {
+    document.body.classList.add('no-scroll');
+  }
+  else {
+    document.body.classList.remove('no-scroll');
+  }
 }
 
-// function closeLogin() {
-//     document.getElementById('overlay').style.display = 'none';
-// }
+
+
+
+function scrollDown() {
+  document.getElementById("cards").scrollIntoView();
+}
